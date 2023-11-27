2fe4f7161dd07504f8250bca2721bbaafe20bfa7
申诺呦 <shennuo123@163.com>
2023-11-27 16:23:56 +0800

    springboot请求响应

---
+++ springboot-web-req-resp/src/main/java/com/example/controller/ResponseController.java
@@ -0,0 +1,88 @@
+package com.example.controller;
+
+import com.example.pojo.Address;
+import com.example.pojo.Result;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 测试响应数据
+ * 响应！
+ */
+@RestController
+public class ResponseController {
+
+//    @RequestMapping("/hello")
+//    public String hello(){
+//        System.out.println("Hello World ~");
+//        return "Hello World ~";
+//    }
+//    // 响应给客户端直接是一个字符串的形式，而使用result封装之后是JSON格式，格式统一，方便解析和管理
+//
+//    @RequestMapping("/getAddr")
+//    public Address getAddr(){
+//        Address addr = new Address();
+//        addr.setProvince("广东");
+//        addr.setCity("深圳");
+//        return addr;
+//    }
+//
+//    @RequestMapping("/listAddr")
+//    public List<Address> listAddr(){
+//        List<Address> list = new ArrayList<>();
+//
+//        Address addr = new Address();
+//        addr.setProvince("广东");
+//        addr.setCity("深圳");
+//
+//        Address addr2 = new Address();
+//        addr2.setProvince("陕西");
+//        addr2.setCity("西安");
+//
+//        list.add(addr);
+//        list.add(addr2);
+//        return list;
+//    }
+
+    /**
+     * 统一响应结果：
+     * 使用result进行封转
+     * 客户端得到一种格式的数据：JSON格式
+     * */
+
+    @RequestMapping("/hello")
+    public Result hello(){
+        System.out.println("Hello World ~");
+        //return new Result(1,"success","Hello World ~");
+        return Result.success("Hello World ~");
+    }
+
+    @RequestMapping("/getAddr")
+    public Result getAddr(){
+        Address addr = new Address();
+        addr.setProvince("广东");
+        addr.setCity("深圳");
+        return Result.success(addr);
+    }
+
+    @RequestMapping("/listAddr")
+    public Result listAddr(){
+        List<Address> list = new ArrayList<>();
+
+        Address addr = new Address();
+        addr.setProvince("广东");
+        addr.setCity("深圳");
+
+        Address addr2 = new Address();
+        addr2.setProvince("陕西");
+        addr2.setCity("西安");
+
+        list.add(addr);
+        list.add(addr2);
+        return Result.success(list);
+    }
+
+
+}
